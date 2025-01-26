7ab7e48521372b7dfd40f829d6383d8ee5af2257
Nabil <nabil_ee@hotmail.com>
2025-01-27 00:51:04 +0100

    remove unused type

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,3 @@
-export type Course = {
-    title: string;
-    level: string;
-    description: string;
-    image: string;
-    slug: string;
-    gradient: string;
-};
-
 export type Event = {
     id: string;
     title: string;
